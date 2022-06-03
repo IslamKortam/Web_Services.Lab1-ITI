17020af76cd18e4c8baa456c2a0055ce862566c2
Islam Kortam <eng.islam.kortam@gmail.com>
2022-06-03 23:34:27 +0200

    done

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,9 @@
+module.exports = (err, req, res, next) => {
+    // console.log('error handler: ', err);
+    res.status(err?.status || 500);
+    res.send({ 
+        message: err?.message,
+        code: err?.code || err?.name,
+        details: err?.details
+    });
+}
